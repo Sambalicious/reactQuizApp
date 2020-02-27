f3da686b2f0ece523451f93e08aadc931db8c452
samuel Temitope <stemitope370@gmail.com>
2020-02-27 14:39:54 +0100

    instructions component almost ready

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,6 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import {LInk} from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import answer from '../assets/img/answer.png';
+import fifty from '../assets/img/fiftyFifty.PNG';
+import hints from '../assets/img/hints.PNG';
+import options from '../assets/img/options.PNG';
+
 
 
 const Instructions = () => {
@@ -12,7 +17,39 @@ const Instructions = () => {
         <div className="instructions">
                <h1>Instructions For the quiz</h1> 
                <p>Ensure that you go over the instructions on this page</p>
+                <ul className="browser-default" id="main-list">
+                    <li> The quiz will play out for exactly 15minutes and ends as soon as your time lapses</li>
+                    <li>In Each quiz, there are 15 questions generated from the server.</li>
+                    <li>Every question has 4 options to choose from.
+                        <img  src={options} alt="options"/>
+                    </li>
+                        <br/><br/>
+                    <li>Select your answer by clicking on one of the options
+                        <br/><br/>
+                        <img src={answer} alt=" demo quiz answer"/>
+                    </li>
+                    <li>For Each quiz, you can have 2 lifelines namely: 
+                        <ul className="sublist">
+                            <li>2 50-50 chances</li>
+                            <li>hints lifeline</li>
+                        </ul>
 
+                    </li> 
+                    <li>Select the Fifty-Fifty chance by clicking on the icon
+                        <span className="mdi mdi-set-center mdi-24px lifeline icon"></span>. 
+                         This leaves you with one right answer and one wrong answer
+                        <img src={fifty} alt="demo fifty fifty options"/>
+                    </li>
+                    <li>Selecting the icon  
+                    <span className="mdi mdi-lightbulb-on mdi-24px lifeline icon"></span>. 
+                        Will remove on wrong answer, one at a time. You can use this option
+                         as many times
+                        on a question
+                        <img src={hints} alt="demo hints options"/>
+                     </li>
+                     <li>You can quit the game at any point by clicking on the quit button</li>
+
+                </ul>
         </div>
         </Fragment>
      );
