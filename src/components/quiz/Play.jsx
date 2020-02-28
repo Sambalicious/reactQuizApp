d1a6833043b2279c68b7be17f53d637b8076098d
samuel Temitope <stemitope370@gmail.com>
2020-02-28 10:01:51 +0100

    added sound to otions clicked

--- src/components/quiz/Play.jsx
+++ src/components/quiz/Play.jsx
@@ -3,6 +3,9 @@ import {Helmet} from 'react-helmet';
 import M from 'materialize-css'
 import questions from '../../questions.json';
 import isEmpty from '../../is-empty';
+import correctSound from '../../assets/audio/correct-answer.mp3';
+import wrongSound from '../../assets/audio/wrong-answer.mp3';
+import buttonSound from '../../assets/audio/button-sound.mp3';
 
 
 class Play extends Component {
@@ -13,7 +16,7 @@ class Play extends Component {
         previousQuestion: {},
         answer:'',
         numberOfQuestions: 0,
-        numberofAnsweredQuestion: 0,
+        numberofAnsweredQuestions: 0,
         currentQuestionIndex:0,
         score:0,
         correctAnswer:0,
@@ -46,10 +49,19 @@ class Play extends Component {
      }
      handleOptionClick = (e) =>{
           if( e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+            setTimeout(() => {
+                document.getElementById('correct-sound').play();
+            },500);
+           
             this.handleCorrectAnswer();
           }
           else{
+              setTimeout(() => {
+                document.getElementById('wrong-sound').play();
+              }, 500);
+            
               this.handleWrongAnswer();
+              
           }
          
      };
@@ -64,8 +76,8 @@ class Play extends Component {
              score: prevState.score + 1,
              correctAnswer: prevState.correctAnswer + 1,
              currentQuestionIndex: prevState.currentQuestionIndex + 1,
-             numberofAnsweredQuestion: prevState.numberofAnsweredQuestion + 1
-         }))
+             numberofAnsweredQuestions: prevState.numberofAnsweredQuestions + 1
+         }),  ()=> {this.displayQuestions(this.state.questions, this.state.currentQuestion, this.state.previousQuestion, this.state.nextQuestion)})
      }
      handleWrongAnswer = () => {
          navigator.vibrate(1000);
@@ -77,8 +89,8 @@ class Play extends Component {
         this.setState(prevState => ({
             wrongAnswers: prevState.wrongAnswers + 1,
             currentQuestionIndex: prevState.currentQuestionIndex + 1,
-            numberofAnsweredQuestion: prevState.numberofAnsweredQuestion + 1
-        }))
+            numberofAnsweredQuestions: prevState.numberofAnsweredQuestions + 1
+        }), ()=> {this.displayQuestions(this.state.questions, this.state.currentQuestion, this.state.previousQuestion, this.state.nextQuestion)})
     }
     render() { 
 
@@ -89,6 +101,13 @@ class Play extends Component {
                 <Helmet>
                     <title> Quiz Page</title>
                 </Helmet>
+
+                <Fragment>
+                    <audio id="correct-sound" src={correctSound} />
+                    <audio id="wrong-sound" src={wrongSound} />
+                    <audio id="button-sound" src={buttonSound} />
+
+                </Fragment>
 
                 <div className="questions">
                     <div className="lifeline-container">
