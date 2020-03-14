35fed8f47516b7fdde660ba781fe7e17982d3375
Sambalicious <stemitope370@gmail.com>
2020-03-14 18:41:34 +0100

    added a loading spinner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {lazy, Suspense } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 const Play = lazy(()=> import('./components/quiz/Play'));
 const QuizSummary = lazy(() => import('./components/quiz/QuizSummary'));
 const Home = lazy(()=> import('./components/Home'));
-const Instructions = lazy(()=> import('./components/Instructions'))
+const Instructions = lazy(()=> import('./components/Instructions'));
+
+
+
+
 const App = () => {
   return (
-    
     <div>
         <Router>
           <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className='spinner'> <h3>Loading... </h3></div>}>
           <Route path="/" exact component={Home} />
           <Route path="/play/instructions" exact component={Instructions}/>
           <Route path="/play/quiz" exact component={Play} />           
